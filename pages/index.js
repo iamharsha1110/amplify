2316e745d7703df9e36137ad2a6f8b645edfb9a6
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-03-19 15:23:57 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! Test Web_Application</a>
+          Welcome to <a href="https://nextjs.org">Harsha!! Test Web_Application</a>
         </h1>
 
         <p className={styles.description}>
